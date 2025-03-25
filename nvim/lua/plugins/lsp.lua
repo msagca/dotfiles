3f00183531b5ca505213250145f307b5513c00a5
msagca <51697294+msagca@users.noreply.github.com>
2025-03-25 20:35:21 +0300

    update keymap descriptions

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -24,7 +24,7 @@ return {
     }
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     require('mason-lspconfig').setup {
-      ensure_installed = { 'clangd', 'glsl_analyzer', 'lua_ls', 'omnisharp_mono', 'pyright', 'rust_analyzer', 'zls' },
+      ensure_installed = { 'clangd', 'glsl_analyzer', 'lua_ls', 'omnisharp', 'pyright', 'rust_analyzer', 'zls' },
       automatic_installation = false,
       handlers = { function(server) require('lspconfig')[server].setup { capabilities = capabilities } end },
     }
