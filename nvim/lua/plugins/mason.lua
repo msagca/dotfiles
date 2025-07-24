6ad6398d606209c987006a98262c12db3022ff56
msagca <51697294+msagca@users.noreply.github.com>
2025-07-24 19:00:18 +0300

    add more language servers

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -6,23 +6,48 @@ return {
     require('mason-tool-installer').setup {
       ensure_installed = {
         'autopep8',
+        'bash-language-server',
         'clang-format',
         'clangd',
+        'css-lsp',
         'gdtoolkit',
         'gersemi',
         'glsl_analyzer',
+        'gopls',
+        'html-lsp',
+        'jdtls',
         'lua-language-server',
         'omnisharp',
         'prettier',
         'pyright',
+        'rust-analyzer',
         'shfmt',
         'stylua',
         'taplo',
+        'typescript-language-server',
         'verible',
+        'yaml-language-server',
+        'zls',
       },
       auto_update = true,
     }
-    local servers = { 'clangd', 'glsl_analyzer', 'lua_ls', 'omnisharp', 'pyright', 'verible' }
+    local servers = {
+      'bashls',
+      'clangd',
+      'cssls',
+      'glsl_analyzer',
+      'gopls',
+      'html',
+      'jdtls',
+      'lua_ls',
+      'omnisharp',
+      'pyright',
+      'rust_analyzer',
+      'ts_ls',
+      'verible',
+      'yamlls',
+      'zls',
+    }
     for _, server in ipairs(servers) do
       vim.lsp.enable(server)
     end
