d19d48b12c920b032579b87d6245f2a57dd974fb
msagca <51697294+msagca@users.noreply.github.com>
2026-02-10 16:06:45 +0300

    disable unused LSPs

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,17 +2,17 @@ require 'bootstrap'
 require 'options'
 require 'mappings'
 require 'commands'
-pcall(function()
-  local lazy = require 'lazy'
-  lazy.setup {
-    require 'plugins.catppuccin',
-    require 'plugins.conform',
-    require 'plugins.lazydev',
-    require 'plugins.mason-lspconfig',
-    require 'plugins.mason-tool-installer',
-    require 'plugins.mini',
-    require 'plugins.oil',
-    require 'plugins.whichkey',
-  }
-  vim.keymap.set('n', '<leader>L', function() lazy.update() end, { desc = 'Lazy update' })
-end)
+pcall(
+  function()
+    require('lazy').setup {
+      require 'plugins.catppuccin',
+      require 'plugins.conform',
+      require 'plugins.lazydev',
+      require 'plugins.mason-lspconfig',
+      require 'plugins.mason-tool-installer',
+      require 'plugins.mini',
+      require 'plugins.oil',
+      require 'plugins.whichkey',
+    }
+  end
+)
--- nvim/lua/plugins/mason-tool-installer.lua
+++ nvim/lua/plugins/mason-tool-installer.lua
@@ -3,23 +3,20 @@ return {
   dependencies = {
     { 'mason-org/mason.nvim', opts = {} },
   },
-  config = function()
-    local installer = require 'mason-tool-installer'
-    installer.setup {
-      ensure_installed = {
-        'black',
-        'clang-format',
-        'gdtoolkit',
-        'gersemi',
-        'ktfmt',
-        'prettier',
-        'shfmt',
-        'sqruff',
-        'stylua',
-        'tex-fmt',
-        'yamlfmt',
-      },
-    }
-    vim.keymap.set('n', '<leader>M', function() installer.check_install(true, false) end, { desc = 'Mason update' })
-  end,
+  opts = {
+    ensure_installed = {
+      'black',
+      'clang-format',
+      'gdtoolkit',
+      'gersemi',
+      'ktfmt',
+      'prettier',
+      'shfmt',
+      'sqruff',
+      'stylua',
+      'taplo',
+      'tex-fmt',
+      'yamlfmt',
+    },
+  },
 }
