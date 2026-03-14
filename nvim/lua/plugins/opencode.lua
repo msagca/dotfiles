782afc6b4e136a523d68d8c0a75ee1d6d79e26d1
msagca <51697294+msagca@users.noreply.github.com>
2026-03-14 12:46:45 +0300

    fix lualine config

--- nvim/lua/plugins/opencode.lua
+++ nvim/lua/plugins/opencode.lua
@@ -6,6 +6,5 @@ return {
     local opencode = require 'opencode'
     vim.keymap.set('n', '<leader>o', function() opencode.toggle() end, { desc = 'Toggle opencode' })
     vim.keymap.set({ 'n', 'x' }, '<c-a>', function() opencode.ask('@this: ', { submit = true }) end, { desc = 'Ask opencode' })
-    vim.keymap.set({ 'n', 'x' }, '<c-x>', function() opencode.select() end, { desc = 'Execute opencode action' })
   end,
 }
