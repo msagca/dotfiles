2d683c7476af62b8a00af8685fac9414b9049ef5
msagca <51697294+msagca@users.noreply.github.com>
2026-03-15 09:21:13 +0300

    fix tabby config

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -5,9 +5,9 @@ return {
       use_default_keymaps = false,
       keymaps = {
         ['-'] = { 'actions.parent', mode = 'n', desc = 'Open parent directory' },
+        ['<c-c>'] = { 'actions.close', mode = 'n', desc = 'Close file explorer' },
         ['<c-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open file in split view' },
         ['<cr>'] = { 'actions.select', desc = 'Open file' },
-        ['<esc>'] = { 'actions.close', mode = 'n', desc = 'Close file explorer' },
         ['<leader>.'] = { 'actions.toggle_hidden', mode = 'n', desc = 'Toggle hidden files' },
         ['<leader>?'] = { 'actions.show_help', mode = 'n', desc = 'Show help' },
         ['<tab>'] = { 'actions.preview', desc = 'Preview file' },
--- nvim/lua/plugins/tabby.lua
+++ nvim/lua/plugins/tabby.lua
@@ -3,5 +3,5 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   event = 'VeryLazy',
   ---@type TabbyConfig
-  opts = { preset = 'active_wins_at_tail', option = { lualine_theme = 'catppuccin' } },
+  opts = { preset = 'active_wins_at_tail', option = { lualine_theme = 'catppuccin-nvim' } },
 }
