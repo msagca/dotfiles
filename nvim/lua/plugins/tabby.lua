e70bfba816544c1c56b0a08136e7096e5258512e
msagca <51697294+msagca@users.noreply.github.com>
2026-05-05 13:52:11 +0300

    remove ps scripts

--- nvim/lua/plugins/tabby.lua
+++ nvim/lua/plugins/tabby.lua
@@ -2,6 +2,5 @@ return {
   'nanozuki/tabby.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   event = 'VeryLazy',
-  ---@type TabbyConfig
   opts = { preset = 'active_wins_at_tail', option = { lualine_theme = 'catppuccin-nvim' } },
 }
